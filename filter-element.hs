286483801d73dce6260673c936089302b97dc2e4
Zhao Jiangkun <jiangkun.zhao@ele.me>
2017-04-12 13:29:14 +0800

    Filter elements using map

--- filter-element.hs
+++ filter-element.hs
@@ -1,10 +1,12 @@
+import Data.List (nub)
+import Data.Map ((!))
+import qualified Data.Map as Map
+
 main :: IO()
 main = do
      line <- getLine
      let n = read line :: Int
      process n
-    
-
 
 process :: Int -> IO()
 process 0 = return ()
@@ -14,24 +16,19 @@ process n = do
 	let kk = getInts l1
 	let a = getInts l2
 	let k = kk !! 1
-	let seq = filter (\(x,n)-> if n>=k then True else False) $ countSeq a
-	if null seq then putStrLn "-1" else putStrLn $ unwords $ map (show . fst) seq
+        let counts = cntSeq a
+        let seq = nub $ filter (\x -> if (Map.findWithDefault 0 x counts) >=k then True else False) $ a
+	if null seq then putStrLn "-1" else putStrLn $ unwords $ map show seq
 	process (n-1)
 
-countSeq :: [Int] -> [(Int,Int)]
-countSeq [] = []
-countSeq all@(x:_) =
-	 let
-		(n,l) = filterElem x all
-	in (x,n) : countSeq l
-
-filterElem :: Int -> [Int] -> (Int,[Int])
-filterElem n [] = (0,[])
-filterElem n (x:xs) =
-	   let
-		(m,ys) = filterElem n xs
-	   in if n==x then (m+1,ys) else (m,x:ys)
-
+cntSeq :: [Int] -> Map.Map Int Int
+cntSeq [] = Map.empty
+cntSeq (x:xs) = let rs = cntSeq xs
+                in  Map.alter ff x rs
+  
+ff :: Maybe Int -> Maybe Int
+ff Nothing = Just 1
+ff (Just n) = Just (n+1)
 
 getInts :: String -> [Int]
 getInts s = map read $ words s

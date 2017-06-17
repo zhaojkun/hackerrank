e236a7aa4c40cb25c7c2107e79c70836357b2971
Zhao Jiangkun <jiangkun.zhao@ele.me>
2017-06-17 22:24:37 +0800

    Update swapNodes

--- swap-nodes.hs
+++ swap-nodes.hs
@@ -11,7 +11,7 @@ main = do
 process :: Tree -> IO Tree
 process t = do
   k <- readLn
-  let t2 = treeSwap t k
+  let t2 = treeSwap k t
   inorderP t2
   putStr "\n"
   return t2
@@ -37,33 +37,29 @@ readLines n = do
 data Tree = Empty | Tree {value::Int ,left::Tree,right::Tree} deriving (Show)
 
 buildTree :: [(Int,Int)]  -> Tree
-buildTree lines = buildTreeHelper lines 1
-
-buildTreeHelper:: [(Int,Int)] -> Int -> Tree
-buildTreeHelper _ (-1) = Empty
-buildTreeHelper lines n =
-  let
-    (leftVal,rightVal) = lines !! (n-1)
-    l = buildTreeHelper lines leftVal
-    r = buildTreeHelper  lines rightVal
-  in Tree n l r
-
-treeSwap :: Tree -> Int -> Tree
-treeSwap t n = treeSwapHelper t n n
-
-treeSwapHelper :: Tree -> Int -> Int -> Tree
-treeSwapHelper Empty _ _ = Empty
-treeSwapHelper (Tree val l r) n 1 =
-  let
-    leftTree = treeSwapHelper r n n
-    rightTree = treeSwapHelper l n n
-  in
-    Tree val leftTree rightTree
-treeSwapHelper (Tree val l r ) n m =
-  let
-    leftTree = treeSwapHelper l n (m-1)
-    rightTree = treeSwapHelper r n (m-1)
-  in
-    Tree val leftTree rightTree
-    
-
+buildTree lines = build  1
+  where
+    build :: Int -> Tree
+    build (-1) = Empty
+    build n =
+      let
+        (leftVal,rightVal) = lines !! (n-1)
+        l = build  leftVal
+        r = build rightVal
+      in Tree n l r
+      
+treeSwap :: Int -> Tree -> Tree
+treeSwap n t = swap n t
+  where
+    swap :: Int  -> Tree -> Tree
+    swap _  Empty = Empty
+    swap 1 (Tree val l r) =
+      let
+        leftTree = swap  n r
+        rightTree = swap n  l
+      in Tree val leftTree rightTree
+    swap m (Tree val l r) =
+      let
+        leftTree = swap (m-1) l
+        rightTree = swap (m-1) r
+      in Tree val leftTree rightTree
